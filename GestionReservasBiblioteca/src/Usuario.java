666d9902a07c507f88958c03e255be1226bec91c
YandroRB <santiago.rdbn@outlook.com>
2024-08-24 16:48:14 -0500

    Creacion de la clase Usuario

---
+++ GestionReservasBiblioteca/src/Usuario.java
@@ -0,0 +1,44 @@
+public class Usuario {
+    private String nombres;
+    private String apellidos;
+    private String identificacion;
+    private String correo="desconocido";
+
+    public Usuario(String nombres,String identificacion,String apellidos){
+        this.nombres=nombres;
+        this.apellidos=apellidos;
+        this.identificacion=identificacion;
+    }
+    public String getNombres(){
+        return this.nombres;
+    }
+    public String getApellidos(){
+        return this.apellidos;
+    }
+    public String getIdentificacion(){
+        return this.identificacion;
+    }
+    public String getCorreo(){
+        return this.correo;
+    }
+    public void setNombres(String nombres){
+        this.nombres=nombres;
+    }
+    public void setApellidos(String apellidos){
+        this.apellidos=apellidos;
+    }
+    public void setCorreo(String correo){
+        this.correo=correo;
+    }
+    @Override
+    public String toString(){
+        return "{\n" +
+                "Identificacion: "+this.identificacion+",\n" +
+                "Nombres: "+this.nombres+",\n" +
+                "Apellidos: "+this.apellidos+",\n" +
+                "Correo: "+this.correo+"\n" +
+                "}";
+
+    }
+
+}
