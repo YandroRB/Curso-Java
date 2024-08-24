f15d8671c16d7111601b83584e8a971a9dca7483
YandroRB <santiago.rdbn@outlook.com>
2024-08-24 17:58:59 -0500

    Creacion de la clase Libro

---
+++ GestionReservasBiblioteca/src/Libro.java
@@ -0,0 +1,92 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class Libro {
+    private String identifLibro;
+    private String titulo;
+    private String autor;
+    private String genero="Desconocido";
+    private Date fechaPublicacion ;
+    private String resumen ="Desconocido";
+
+
+    public Libro (String identifLibro, String titulo, String autor, String fechaPublicacion){
+        this.identifLibro=identifLibro;
+        this.titulo=titulo;
+        this.autor=autor;
+        try{
+            SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+            this.fechaPublicacion = format.parse(fechaPublicacion);
+        }
+        catch (ParseException e){
+            System.err.println(e.getMessage());
+        }
+
+    }
+
+    public Libro(String identifLibro, String titulo, String genero, String autor, String resumen, String fechaPublicacion) {
+        this.identifLibro = identifLibro;
+        this.titulo = titulo;
+        this.genero = genero;
+        this.autor = autor;
+        this.resumen = resumen;
+        try{
+            SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+            this.fechaPublicacion = format.parse(fechaPublicacion);
+        }
+        catch (ParseException e){
+            System.err.println(e.getMessage());
+        }
+    }
+
+    public String getIdentifLibro(){
+        return this.identifLibro;
+    }
+    public String getTitulo(){
+        return this.titulo;
+    }
+    public  String getAutor(){
+        return this.autor;
+    }
+    public String getGenero() {
+        return genero;
+    }
+    public Date getFechaPublicacion(){
+        return fechaPublicacion;
+    }
+    public String getResumen(){
+        return resumen;
+    }
+    public void  setTitulo(String titulo){
+        this.titulo=titulo;
+    }
+    public void setAutor(String autor){
+        this.autor=autor;
+    }
+    public void setResumen(String resumen) {
+        this.resumen = resumen;
+    }
+    public void setFechaPublicacion(Date fechaPublicacion) {
+        this.fechaPublicacion = fechaPublicacion;
+    }
+
+
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    @Override
+    public String toString() {
+        SimpleDateFormat format = new SimpleDateFormat("dd 'de' MMMM 'del' yyyy", Locale.of("es","ES"));
+        return "{\n" +
+                "identifLibro='" + identifLibro + ",\n" +
+                "titulo='" + titulo + ",\n" +
+                "autor='" + autor + ",\n" +
+                "fechaPublicacion=" + format.format(fechaPublicacion) + ",\n"+
+                "resumen='" + resumen + ",\n" +
+                '}';
+    }
+}
--- GestionReservasBiblioteca/src/Main.java
+++ GestionReservasBiblioteca/src/Main.java
@@ -1,7 +1,4 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -12,9 +9,21 @@ public class Main {
             add(new Usuario("Ana Carme","65321","Salazar Perez"));
             add(new Usuario("Laura Gabriela","21453","Lopez Gomez"));
         }};
-        usuarios.add(new Usuario("Gabriela Maria","12124","Salazar Jaramillo"));
+        usuarios.add(new Usuario("Gabriela Maria","12124","Salazar Jaramillo","gmaria@gmail.com"));
+
+        List<Libro> libros = new ArrayList<>(){{
+           add(new Libro("56231","Cien años de soledad","Gabriel García Márquez","5/6/1967"));
+           add(new Libro("56531","1984","George Orwell","8/6/1949"));
+           add(new Libro("56924","El gran Gatsby","F. Scott Fitzgerald","10/4/1925"));
+           add(new Libro("56842","Orgullo y prejuicio","Jane Austen","28/1/1813"));
+           add(new Libro("56231","Los juegos del hambre","Suzanne Collins","14/9/2008"));
+        }};
+
         for(Usuario usuario:usuarios){
             System.out.println(usuario);
+        }
+        for (Libro libro:libros){
+            System.out.println(libro);
         }
     }
 }
