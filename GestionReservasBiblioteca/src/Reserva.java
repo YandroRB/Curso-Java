4465a3715f1a95fd60b14a344efb2ecaaeb57515
YandroRB <santiago.rdbn@outlook.com>
2024-08-26 15:35:11 -0500

    Modificacion del contructor de la clase Reserva

--- GestionReservasBiblioteca/src/Reserva.java
+++ GestionReservasBiblioteca/src/Reserva.java
@@ -5,10 +5,10 @@ public class Reserva {
     private Usuario usuario;
     private ReservarLibro[] libros = new ReservarLibro[5];
 
-    public Reserva(String identifReserva, Usuario usuario, ReservarLibro[] libros) {
+
+    public Reserva(String identifReserva, Usuario usuario) {
         this.identifReserva = identifReserva;
         this.usuario = usuario;
-        this.libros = libros;
     }
 
     public String getIdentifReserva() {
